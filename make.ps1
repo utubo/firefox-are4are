73a6186df7879d736b80f9385f29b15e9c334454
utubo <utbutb@gmail.com>
2016-05-15 17:50:52 +0900

    Firefox for Androidにインストール出来ない問題を修正

--- make.ps1
+++ make.ps1
@@ -1,3 +1,26 @@
-Compress-Archive src\* are.zip
+# ↓Compress-Archiveで圧縮するとインストールできない…
+# Compress-Archive src\* ..\are.zip
+
+function Add-Zip
+{
+    param([string]$zipfilename)
+
+    if(-not (test-path($zipfilename)))
+    {
+        set-content $zipfilename ("PK" + [char]5 + [char]6 + ("$([char]0)" * 18))
+        (dir $zipfilename).IsReadOnly = $false
+    }
+
+    $shellApplication = new-object -com shell.application
+    $zipPackage = $shellApplication.NameSpace($zipfilename)
+
+    foreach($file in $input)
+    {
+            $zipPackage.CopyHere($file.FullName)
+            Start-sleep -milliseconds 500
+    }
+}
+
+dir src\* | Add-Zip ((Convert-Path .) + '\are.zip')
 Move-Item are.zip are.xpi -Force
 
