393431d4a40cc5f27e41ba5e189f31a3ead42093
utubo <utbutb@gmail.com>
2016-11-15 12:22:19 +0900

    デフォルトでhttpsにも適用するようにしました

--- src/content_scripts/all_urls.js
+++ src/content_scripts/all_urls.js
@@ -7,7 +7,7 @@ chrome.storage.local.get('urls', r => {
 	// Check URL
 	let href = document.location.href;
 	let is1stPage = false;
-	if (href.indexOf('mode=cat') === -1 && href.match(/^http:\/\/[a-z]{3}\.2chan\.net\/[a-z\d]+\/(res\/\d+|futaba\.htm|\d+\.htm)/)) {
+	if (href.indexOf('mode=cat') === -1 && href.match(/^https?:\/\/[a-z]{3}\.2chan\.net\/[a-z\d]+\/(res\/\d+|futaba\.htm|\d+\.htm)/)) {
 		is1stPage = href.indexOf('/res/') === -1;
 	} else if (href.match(/\/cont\/[a-z]{3}\.2chan\.net_[a-z\d]+_res_\d+\/index.htm$/)) {
 		// FTBucket
